0e0cf88125970164778545e77833af6692a109a0
isakordis <30376108+isakordis@users.noreply.github.com>
2019-06-27 22:11:32 +0300

    Add files via upload

---
+++ AbstractFactory.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama9
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //===============>>Abstract Factory
+            UrunFactory ur = new Ols();
+            ur.createHermes();
+            ur.createGucci();
+            ur.createChanel();
+            Console.ReadLine();
+        }
+    }
+
+
+    public interface UrunFactory
+    {
+        Gucci createGucci();
+        Hermes createHermes();
+        Chanel createChanel();
+
+    }
+    public interface Gucci { }
+    public interface Hermes { }
+    public interface Chanel { }
+
+    public class Ols:UrunFactory
+    {
+        public Gucci createGucci()
+        {
+
+            return new _Gucci();
+        }
+        public Hermes createHermes()
+        {
+            return new _Hermes();
+        }
+        public Chanel createChanel()
+        {
+            return new _Chanel();
+        }
+
+       
+    }
+    public class _Gucci:Gucci
+    {
+        string sonuc= "Gucci Urunu 2017 model 50 Tl";
+        public _Gucci()
+        {
+            Console.WriteLine(sonuc);
+        }
+        public string donder()
+        {
+            return sonuc;
+        }
+    }
+    public class _Hermes : Hermes
+    {
+        string sonuc = "Hermes Urunu 2017 model 50 Tl";
+        public _Hermes()
+        {
+            Console.WriteLine(sonuc);
+        }
+        public string donder()
+        {
+            return sonuc;
+        }
+    }
+    public class _Chanel : Chanel
+    {
+        string sonuc = "Chanel Urunu 2017 model 50 Tl";
+        public _Chanel()
+        {
+            Console.WriteLine(sonuc);
+        }
+        public string donder()
+        {
+            return sonuc;
+        }
+    }
+
+}
+    
+   
+
+
---
+++ Factory.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soru2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //===============>>Factory
+            for (int i = 1; i <= 30; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    IMantarGonder mantar = new MantarIsp();
+                    mantar.Operation();
+                    Console.WriteLine(i.ToString());
+                }
+                else if(i%11==0)
+                {
+                    IMantarGonder mantar = new MantarIsv();
+                    mantar.Operation();
+                    Console.WriteLine(i.ToString());
+                }
+                else
+                {
+                    IMantarGonder mantar = new MantarIng();
+                    mantar.Operation();
+                    Console.WriteLine(i.ToString());
+                }
+            }
+            Console.ReadLine() ;
+        }
+    }
+   
+    public abstract class gun_belirtec
+    {
+      public abstract IMantarGonder GetMantar(); 
+    }
+    public abstract class IMantarIsv:gun_belirtec
+    {
+        public override IMantarGonder GetMantar()
+        {
+            return new MantarIsv();
+        }
+
+    }
+    public abstract class IMantarIsp : gun_belirtec
+    {
+        public override IMantarGonder GetMantar()
+        {
+            return new MantarIsp();
+        }
+    }
+    public abstract class IMantarIng : gun_belirtec
+    {
+        public override IMantarGonder GetMantar()
+        {
+            return new MantarIng();
+        }
+    }
+    public interface IMantarGonder
+    {
+        void Operation();
+    }
+    class MantarIsv : IMantarGonder
+    {
+        public void Operation()
+        {
+            Console.WriteLine( "İsviçre Mantarı Gönderildi...");
+        }
+    }
+    class MantarIsp : IMantarGonder
+    {
+        public void Operation()
+        {
+            Console.WriteLine( "İspanya Mantarı Gönderildi...");
+        }
+    }
+    class MantarIng : IMantarGonder
+    {
+        public void Operation()
+        {
+            Console.WriteLine( "İngiltere Mantarı Gönderildi...");
+        }
+    }
+}
